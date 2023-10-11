c26888e5cbfc321eea5f394cff429d0735c65a6c
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2023-10-11 13:30:08 +0700

    upload

---
+++ src/Perbankan/BankTelkom.java
@@ -0,0 +1,13 @@
+package Perbankan;
+
+//2.5 TEST AKHIR
+// Buatlah sebuah aplikasi Perbankkan sederhana, yang dapat melakukan beberapa proses berikut ini.
+//      a. Penambahan rekening baru
+//      b. Lihat Saldo,
+//      c. Menambah saldo,
+//      d. Menarik saldo.
+// Catatan :
+//  1. Tentukan parameter-parameter yang dibutuhkan pada setiap fungsionalitas diatas.
+//  2. Proses pengecekan kecukupan saldo belum dibutuhkan pada bagian ini.
+public class BankTelkom {
+}
